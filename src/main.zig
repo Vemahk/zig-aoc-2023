de7cf8b866758ba0c4c939475c8e5257d8175472
Vemahk <vemahk20@gmail.com>
2023-12-02 22:59:16 -0600

    it's time

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,9 @@
 const std = @import("std");
 
-pub fn main() !void {}
+pub fn main() !void {
+    const cwd = std.fs.cwd();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const a = gpa.allocator();
+    const contents = try cwd.readFileAlloc(a, "inputs/day-three.txt", 1 << 20);
+    defer a.free(contents);
+}
