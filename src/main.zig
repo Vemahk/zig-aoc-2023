8728ac422f91ac19aaf0e6cc8db7ea4cf5bb37c1
Vemahk <vemahk20@gmail.com>
2023-12-05 21:51:02 -0600

    day four part two

--- src/main.zig
+++ src/main.zig
@@ -7,12 +7,17 @@ pub fn main() !void {
     const contents = try cwd.readFileAlloc(a, "inputs/day-four.txt", 1 << 20);
     defer a.free(contents);
 
-    var point_sum: usize = 0;
+    var card_count = CardCounter.init(a);
+    defer card_count.deinit();
+
     var line_iter = std.mem.tokenizeAny(u8, contents, "\n\r");
     var buf = std.ArrayList(usize).init(a);
+    defer buf.deinit();
 
-    while (line_iter.next()) |line| {
+    var card_num: usize = 0;
+    while (line_iter.next()) |line| : (card_num += 1) {
         buf.clearRetainingCapacity();
+        try card_count.add(card_num);
         var card_iter = std.mem.splitSequence(u8, line, ": ");
         _ = card_iter.next() orelse unreachable;
         const card = card_iter.next() orelse unreachable;
@@ -32,17 +37,56 @@ pub fn main() !void {
                 try buf.append(n);
             } else {
                 if (std.mem.indexOf(usize, buf.items, &[_]usize{n})) |_| {
-                    if (value == 0) {
-                        value = 1;
-                    } else {
-                        value = value << 1;
-                    }
+                    value += 1;
                 }
             }
         }
 
-        point_sum += value;
+        try card_count.win(card_num, value);
     }
 
-    std.log.info("point sum: {d}", .{point_sum});
+    std.log.info("point sum: {d}", .{card_count.sum()});
 }
+
+const CardCounter = struct {
+    cards: std.ArrayList(usize),
+
+    pub fn init(a: std.mem.Allocator) CardCounter {
+        return .{
+            .cards = std.ArrayList(usize).init(a),
+        };
+    }
+
+    fn ensure(self: *CardCounter, card: usize) !void {
+        while (self.cards.items.len <= card) {
+            try self.cards.append(0);
+        }
+    }
+
+    pub fn add(self: *CardCounter, card: usize) !void {
+        try self.ensure(card);
+        self.cards.items[card] += 1;
+    }
+
+    pub fn win(self: *CardCounter, card: usize, num: usize) !void {
+        try self.ensure(card + num);
+        const c = self.cards.items[card];
+
+        var i: usize = 0;
+        while (i < num) : (i += 1) {
+            self.cards.items[card + i + 1] += c;
+        }
+    }
+
+    pub fn sum(self: CardCounter) usize {
+        var s: usize = 0;
+        for (self.cards.items) |card| {
+            s += card;
+        }
+        return s;
+    }
+
+    pub fn deinit(self: CardCounter) void {
+        self.cards.deinit();
+    }
+};
